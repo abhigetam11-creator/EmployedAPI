7833a9678ae5b58dbea4b36d3f25300b5f5c85af
Abhi Getam <abhigetam11@gmail.com>
2026-07-02 12:50:29 +0530

    Add project files.

---
+++ EmployedAPI/Controllers/EmployedController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeAPI.Models;
+
+namespace EmployeeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public Employee GetEmployee()
+        {
+            return new Employee
+            {
+                Id = 1,
+                Name = "Abhi",
+                Department = "IT"
+            };
+        }
+    }
+}
